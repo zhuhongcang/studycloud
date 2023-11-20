0c9a33637e87be1db978af8a0c3ac8808d1a25e6
zhuhongcang <928636416@qq.com>
2023-11-20 19:05:09 +0800

    test1111

--- eureka-server7001/src/main/java/com/zhc/cloud/eureka/EurekaApplication7001.java
+++ eureka-server7001/src/main/java/com/zhc/cloud/eureka/EurekaApplication7001.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
- *
+ * EurekaApplication7001
  * @author zhuhongcang
  * @date 2023/11/20
  */
