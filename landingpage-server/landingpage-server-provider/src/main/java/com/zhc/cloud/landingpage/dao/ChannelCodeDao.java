b1bb4790da6ef75885e8bd153f2e0c56bb77ef8c
zhuhongcang <928636416@qq.com>
2023-11-21 16:08:24 +0800

    init

--- landingpage-server/landingpage-server-provider/src/main/java/com/zhc/cloud/landingpage/dao/ChannelCodeDao.java
+++ landingpage-server/landingpage-server-provider/src/main/java/com/zhc/cloud/landingpage/dao/ChannelCodeDao.java
@@ -2,10 +2,12 @@ package com.zhc.cloud.landingpage.dao;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.zhc.cloud.landingpage.entity.ChannelCode;
+import org.apache.ibatis.annotations.Mapper;
 
 /**
  * @author zhuhongcang
  * @date 2023/11/21
  */
+@Mapper
 public interface ChannelCodeDao extends BaseMapper<ChannelCode> {
 }
