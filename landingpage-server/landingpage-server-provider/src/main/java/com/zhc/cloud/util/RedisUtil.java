28c33f343dcb456367b079c1dc14c10f305e6a70
zhuhongcang <928636416@qq.com>
2023-11-22 19:09:08 +0800

    init

--- landingpage-server/landingpage-server-provider/src/main/java/com/zhc/cloud/util/RedisUtil.java
+++ landingpage-server/landingpage-server-provider/src/main/java/com/zhc/cloud/util/RedisUtil.java
@@ -1,5 +1,6 @@
 package com.zhc.cloud.util;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.util.CollectionUtils;
@@ -12,6 +13,7 @@ import java.util.concurrent.TimeUnit;
  * @author zhuhongcang
  * @date 2023/11/22
  */
+@Slf4j
 @Component
 public class RedisUtil {
     @Resource
@@ -19,6 +21,7 @@ public class RedisUtil {
 
     /**
      * 删除缓存
+     *
      * @param key 一个或多个值
      * @return 成功删除个数
      */
@@ -35,9 +38,9 @@ public class RedisUtil {
         return 0;
     }
 
-
     /**
      * 删除缓存
+     *
      * @param key key集合
      * @return 成功删除个数
      */
@@ -90,28 +93,125 @@ public class RedisUtil {
     }
 
     // region String
+
+    /**
+     * 普通缓存获取
+     *
+     * @param key 键
+     * @return 值
+     */
     public Object get(String key) {
         return key == null ? null : redisTemplate.opsForValue().get(key);
     }
 
+    /**
+     * 普通缓存放入
+     *
+     * @param key   键
+     * @param value 值
+     * @return true成功 false失败
+     */
     public boolean set(String key, Object value) {
         try {
             redisTemplate.opsForValue().set(key, value);
             return true;
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error(e.getMessage(), e);
             return false;
         }
     }
+
+    /**
+     * 普通缓存setnx放入
+     * 如果不存在返回true，已存在返回false
+     *
+     * @param key   键
+     * @param value 值
+     * @return true成功 false失败
+     */
     public boolean setnx(String key, Object value, long time) {
         Boolean result = false;
         try {
             result = redisTemplate.opsForValue().setIfAbsent(key, value, time, TimeUnit.SECONDS);
         } catch (Exception e) {
-            e.printStackTrace();
+            log.error(e.getMessage(), e);
             return result;
         }
         return result;
+    }
+
+    /**
+     * 普通缓存放入并设置时间
+     *
+     * @param key   键
+     * @param value 值
+     * @param time  时间(秒) time要大于0 如果time小于等于0 将设置无限期
+     * @return true成功 false 失败
+     */
+    public boolean set(String key, Object value, long time) {
+        try {
+            if (time > 0) {
+                redisTemplate.opsForValue().set(key, value, time, TimeUnit.SECONDS);
+            } else {
+                set(key, value);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error(e.getMessage(), e);
+            return false;
+        }
+    }
+
+    /**
+     * 普通缓存放入并设置时间
+     *
+     * @param key   键
+     * @param value 值
+     * @param time  时间(秒) time要大于0 如果time小于等于0 将设置无限期
+     * @return true成功 false 失败
+     */
+    public boolean set(String key, Object value, long time, TimeUnit timeUnit) {
+        try {
+            if (time > 0) {
+                redisTemplate.opsForValue().set(key, value, time, timeUnit);
+            } else {
+                set(key, value);
+            }
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            log.error(e.getMessage(), e);
+            return false;
+        }
+    }
+
+    /**
+     * 递增
+     *
+     * @param key   键
+     * @param delta 要增加几(大于0)
+     * @return
+     */
+    public long incr(String key, long delta) {
+        if (delta < 0) {
+            throw new RuntimeException("递增因子必须大于0");
+        }
+        return redisTemplate.opsForValue().increment(key, delta);
+    }
+
+    /**
+     * 递减
+     *
+     * @param key   键
+     * @param delta 要减少几(小于0)
+     * @return
+     */
+    public long decr(String key, long delta) {
+        if (delta < 0) {
+            throw new RuntimeException("递减因子必须大于0");
+        }
+        return redisTemplate.opsForValue().increment(key, -delta);
     }
     // endregion
 }
